cb5d8c3f14d455f77b57a7bb02a8e3df962c3a7b
Unknown <danimaejanssen@gmail.com>
2019-04-29 17:23:46 -0500

    test with path

--- .appveyor/build.ps1
+++ .appveyor/build.ps1
@@ -1,4 +1,4 @@
 
-ls "C:\Program Files (x86)\WiX Toolset v3.11\bin\"
-# cd windows\Installer
-# .\build.ps1
+$env:path = "C:\Program Files (x86)\WiX Toolset v3.11\bin\;" + $env:path
+cd windows\Installer
+.\build.ps1
